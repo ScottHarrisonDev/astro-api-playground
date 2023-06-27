cc36f8e1a6d4967966f2935acc92dd7af8886ece
Scott Harrison <github@scottharrisondesign.co.uk>
2023-06-27 14:55:50 +0100

    basic astro fetching from api and displaying pages

---
+++ src/types/drink.ts
@@ -0,0 +1,12 @@
+export type Drink = {
+    strDrink: string;
+    strDrinkThumb: string;
+    idDrink: string;
+}
+
+export type DecoratedDrink = {
+    strDrink: string;
+    strDrinkThumb: string;
+    idDrink: string;
+    href: string;
+}
